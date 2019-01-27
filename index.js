ff03935d86a8b48f2070a45c1a0791704bc7bf61
Yasmine Hartung <y.hartung@gmail.com>
2019-01-27 08:01:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 function theBeatlesPlay (musician, instrument) {
-  var beatlesMembers = []
+  var beatlesMembers = [];
+  var i;
   for (i=0; i < musicians.length; i++){
     beatlesMembers[i] = `${musician[i]} plays ${instrument[i]}`
   }
-
+  
 }
 // add solution here
